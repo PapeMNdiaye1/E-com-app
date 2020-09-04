7784481bafb6e15d1180a2abead55a0757dbbee2
Pape Ndiaye <pmomar58@gmail.com>
2020-09-04 13:19:28 +0000

    First Commit

---
+++ my-app/src/main/Carousel/Dots.js
@@ -0,0 +1,38 @@
+/** @jsx jsx */
+import React from "react";
+import { css, jsx } from "@emotion/core";
+
+const Dot = ({ active }) => (
+  <div
+    css={css`
+      padding: 10px;
+      margin-right: 1px;
+      cursor: pointer;
+      border-radius: 50%;
+      background: ${active ? "black" : "white"};
+
+      transform: scale(0.5);
+    `}
+  >
+    {" "}
+  </div>
+);
+
+const Dots = ({ slides, activeSlide }) => (
+  <div
+    css={css`
+      position: absolute;
+      bottom: 25px;
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    `}
+  >
+    {slides.map((slide, i) => (
+      <Dot key={slide} active={activeSlide === i} />
+    ))}
+  </div>
+);
+
+export default Dots;
---
+++ my-app/src/main/Carousel/Slide.js
@@ -0,0 +1,17 @@
+/** @jsx jsx */
+// import React from "react";
+import { css, jsx } from "@emotion/core";
+
+const Slide = ({ content }) => {
+  const SlideCSS = css`
+    height: 100%;
+    width: 100%;
+    background-image: url("${content}");
+    background-size: 100%;
+    background-repeat: no-repeat;
+    background-position: center;
+  `;
+  return <div css={SlideCSS}></div>;
+};
+
+export default Slide;
